eab1a0af18a36893fe065c1c152f93a1622cbe35
zz <zz.at.field@gmail.com>
2016-04-17 20:50:30 +0800

    try

--- rasp.py
+++ rasp.py
@@ -1,17 +1,38 @@
 import RPi.GPIO as GPIO
+from threading import Thread
 
 led_pin = 17
+sr_trigger = 18
+sr_echo = 23
+
 
 class RaspControler:
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(led_pin, GPIO.OUT)  # LED pin set as output
+        GPIO.setup(sr_trigger, GPIO.OUT)
+        GPIO.setup(sr_echo, GPIO.IN)
+        self.pwm = GPIO.PWM(sr_trigger, 10)
 
     def led_on(self):
         GPIO.output(led_pin, GPIO.HIGH)
 
     def led_off(self):
         GPIO.output(led_pin, GPIO.LOW)
+
+    def sr_start(self):
+        self.pwm.start(1)
+
+    def debug_output(self):
+        def output():
+            import time
+            while True:
+                print(int(GPIO.input(sr_echo)))
+                time.sleep(0.001)
+
+        self.t = Thread(target=output)
+        self.t.start()
+
 
 
 rasp = RaspControler()
